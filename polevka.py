185bc3de5186ca854f64b36c4970830677804697
JS <souhrada@trebesin.cz>
2026-01-04 20:07:34 +0100

    oprava

--- polevka.py
+++ polevka.py
@@ -1,22 +1,26 @@
 from bs4 import BeautifulSoup
 import requests
 import json
-#from recept import *
+
+# from recept import *
 
 
 def main():
 
-
-    response = requests.get('https://souhrada.github.io/bsoup-exam/')
+    response = requests.get("https://souhrada.github.io/bsoup-exam/")
 
     soup = BeautifulSoup(response.content, "html.parser")
-    ingredience = soup.select('h2')
-    i = ingredience[0].text, ingredience[1].text, ingredience[2].text, ingredience[3].text
+    ingredience = soup.select("h2")
+    i = (
+        ingredience[0].text,
+        ingredience[1].text,
+        ingredience[2].text,
+        ingredience[3].text,
+    )  # co je toto za syntax??
     print(i)
 
-    json.load('recept.js')
-    json.dump(i, 'recept.js', indent=4)
-     
+    json.load("recept.js")  # tohle načítá z jsonu - načítáte něco z jsonu?
+    json.dump(i, "recept.js", indent=4)  # chybí syntax s with open
 
 
 if __name__ == "__main__":
